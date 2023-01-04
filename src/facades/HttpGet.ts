f679034f73c52c4e648417a35b3920ba1ca17eac
Alexander Bird <alex@alexanderbird.software>
2023-01-03 17:58:18 -0800

    Add some rudimentary error handling in HttpGet wrapper

--- src/facades/HttpGet.ts
+++ src/facades/HttpGet.ts
@@ -9,13 +9,28 @@ export interface HttpGetRequest {
 }
 
 export interface SimpleFetch {
-  (url: string, options: { headers: { [key: string]: string } }): Promise<{ json: () => Promise<{}> }>;
+  (url: string, options: { headers: { [key: string]: string } }): Promise<FetchResponse>;
+}
+
+export interface FetchResponse {
+  json: () => Promise<{}>;
+  text: () => Promise<string>;
+  ok: boolean;
+  status: number;
+  statusText: string;
 }
 
 export function wrapFetch(fetch: SimpleFetch): HttpGet {
   return ({ url, queryParams, headers }) =>
     fetch(url + "?" + stringifyQueryString(queryParams), { headers })
-      .then(x => x.json());
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        return response.text().then(details => {
+          throw new Error(`HTTP ${response.status} ${response.statusText}: ${details}`);
+        });
+      });
 }
 
 function stringifyQueryString(params: { [key: string]: string }) {
